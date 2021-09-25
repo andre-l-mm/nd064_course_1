3f0003af69e31babccc0dfc0f303588778f77116
Andre Magalhaes <andre.l.magalhaes@gmail.com>
2021-09-25 07:14:43 -0400

    Step 1 - Completed addition of metrics endpoint, healthz endpoint and logging

--- project/techtrends/README.md
+++ project/techtrends/README.md
@@ -8,3 +8,35 @@ To run this application there are 2 steps required:
 
 1. Initialize the database by using the `python init_db.py` command. This will create or overwrite the `database.db` file that is used by the web application.
 2.  Run the TechTrends application by using the `python app.py` command. The application is running on port `3111` and you can access it by querying the `http://127.0.0.1:3111/` endpoint.
+
+
+## Commands Used
+```
+# Create virtual environment
+python3 -m venv .venv
+
+# Activate virtual environment
+cd .venv
+source bin/activate
+
+# Initialize the database
+python init_db.py
+
+# Upgrade pip
+pip install --upgrade pip
+
+# Install required packages
+pip install -r requirements.txt
+
+# Start the application
+python app.py
+
+# Go to http://127.0.0.1:3111/
+
+# Using flask command line to start the application
+# This can be used to automatically apply source code changes but runs on port 5000 instead of 3111
+FLASK_APP=app.py FLASK_ENV=development flask run
+
+# Go to http://127.0.0.1:5000/
+```
+
--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,28 +1,50 @@
+import sys
 import sqlite3
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
+import logging
+
+connection_count = 0
+
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
+    global connection_count
+
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+
+    connection_count += 1
+
     return connection
+
 
 # Function to get a post using its ID
 def get_post(post_id):
     connection = get_db_connection()
     post = connection.execute('SELECT * FROM posts WHERE id = ?',
-                        (post_id,)).fetchone()
+                              (post_id,)).fetchone()
     connection.close()
     return post
+
+
+# Count the number of posts in the database
+def count_posts():
+    connection = get_db_connection()
+    count = connection.execute('SELECT count(1) FROM posts').fetchone()
+    connection.close()
+
+    return count
+
 
 # Define the Flask application
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
 
-# Define the main route of the web application 
+
+# Define the main route of the web application
 @app.route('/')
 def index():
     connection = get_db_connection()
@@ -30,22 +52,28 @@ def index():
     connection.close()
     return render_template('index.html', posts=posts)
 
-# Define how each individual article is rendered 
+
+# Define how each individual article is rendered
 # If the post ID is not found a 404 page is shown
 @app.route('/<int:post_id>')
 def post(post_id):
     post = get_post(post_id)
     if post is None:
-      return render_template('404.html'), 404
+        app.logger.debug('Article {} does not exist!'.format(post_id))
+        return render_template('404.html'), 404
     else:
-      return render_template('post.html', post=post)
+        app.logger.debug('Article {} retrieved!'.format(post['title']))
+        return render_template('post.html', post=post)
+
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    app.logger.debug('About Us page retrieved!')
     return render_template('about.html')
 
-# Define the post creation functionality 
+
+# Define the post creation functionality
 @app.route('/create', methods=('GET', 'POST'))
 def create():
     if request.method == 'POST':
@@ -57,14 +85,67 @@ def create():
         else:
             connection = get_db_connection()
             connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
-                         (title, content))
+                               (title, content))
             connection.commit()
             connection.close()
+            app.logger.debug('Article titled "{}" created!'.format(title))
 
             return redirect(url_for('index'))
 
     return render_template('create.html')
 
+
+# Define the health monitor endpoint
+@app.route("/healthz")
+def healthz():
+    response = app.response_class(
+        response=json.dumps({"result": "OK - healthy"}),
+        status=200,
+        mimetype='application/json'
+    )
+
+    return response
+
+
+# Define the metrics endpoint
+@app.route("/metrics")
+def metrics():
+    response = app.response_class(
+        response=json.dumps({
+            "db_connection_count": connection_count,
+            "post_count": count_posts()[0],
+        }),
+        status=200,
+        mimetype='application/json'
+    )
+
+    return response
+
+
+# Configure default logging to log to both stdout and stderror as requested.
+# This will duplicate all log lines on the terminal but it is what is being asked in the assignement.
+# The format used is the same as logging.BASIC_FORMAT with the addition of a timestamp between brackets.
+def configure_log():
+    format = '%(levelname)s:%(name)s:[%(asctime)s]:%(message)s'
+    level = logging.DEBUG
+    handlers = [get_handler(sys.stdout, format, level),
+                get_handler(sys.stderr, format, level)]
+    logging.basicConfig(level=level, handlers=handlers)
+
+
+# Creates a new stream logging handler with the provided stream, format and logging level.
+def get_handler(stream, format, level):
+    handler = logging.StreamHandler(stream)
+    handler.setLevel(level)
+    formatter = logging.Formatter(format)
+    handler.setFormatter(formatter)
+
+    return handler
+
+
+# configure log outputing to both stdout and stderr as requested - This generates duplicated logs on terminal
+configure_log()
+
 # start the application on port 3111
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port='3111')
+    app.run(host='0.0.0.0', port='3111')
